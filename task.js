f12e9da06eb4f9aa0463bc0528794b4837da1f2e
Dima Shirkov <dimashirkov@list.ru>
2022-08-03 06:07:50 +0300

    dak1 draft v2

--- task.js
+++ task.js
@@ -1,7 +1,8 @@
 const img = document.querySelector('img'); // доступ к изображению
+let rateListWrapper = document.getElementById('items'); // доступ к обертке списка валют
 const currencyExchangeClone = document.querySelector('.item'); // константа для клонирования
 currencyExchangeClone.setAttribute('style','display: none;')
-console.log(img)
+
 
 let xhr = new XMLHttpRequest(); // экземпляр объекта XMLHttpRequest  
 xhr.open('GET', 'https://netology-slow-rest.herokuapp.com'); // инициализация запроса
@@ -10,26 +11,25 @@ xhr.send();
 xhr.onload = function() {
     img.classList.remove('loader_active'); // удаление изображения загрузки
     const result = JSON.parse(xhr.response); // изменение типа данных результата ответа сервера из строки в объект
+    
+    // серия итераций для погружения в JSON объект
     for (const [key, value] of Object.entries(result.response)) {
         if (key === 'Valute') {
             for (const [currency, rate] of Object.entries(value)) {
                 let itemCurrency = currencyExchangeClone.cloneNode(true); //клонирование тега
+
                 Object.entries(rate).forEach(([key, value]) => {
-                    // console.log(`${key} ${value}`)
                     if(key === 'CharCode') {
                         itemCurrency.querySelector('.item__code').textContent = value;
                     } else if (key === 'Value') {
                         itemCurrency.querySelector('.item__value').textContent = value;
                     }
                 })
-                console.log(itemCurrency)
-                
-                // rate.firstElementChild.textContent = key;
-                // console.log(rate);
-            }
-            
+
+                itemCurrency.setAttribute('style', 'display: flex'); // переопределние display
+                rateListWrapper.appendChild(itemCurrency); // добавление элемента на страницу
+            }        
         }
     };
-    // console.log(typeof rr)
 }
 
